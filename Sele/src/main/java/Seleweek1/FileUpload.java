a054b4c0e6d68fefbc6a58c205b1456021dc0d73
reshma.s02 <reshma.s02@BCIT-WIN-S1410.simplify3x.com>
2024-10-24 13:17:02 +0530

    file upload

--- Sele/src/main/java/Seleweek1/FileUpload.java
+++ Sele/src/main/java/Seleweek1/FileUpload.java
@@ -25,28 +25,28 @@ public class FileUpload {
 				Actions click= new Actions(driver);
 				click.moveToElement(fileupload).click().perform();
 				StringSelection ss = new StringSelection("C:\\Users\\reshma.s02\\Downloads\\SELEDEMO.xlsx");  //storing the file to ss
-		   	     Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+		   	    Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
 		   
-		     Robot robot = new Robot(); //command for pasting 
-		     //robot.delay(350);
+		    Robot robot = new Robot(); //command for pasting 
+		     robot.delay(2000);
 		     robot.keyPress(KeyEvent.VK_CONTROL); //press ctrl key"C:\Users\reshma.s02\Downloads\RESHMA.S._M_Senior_Software_Test_Engineer.pdf"
 		     
 		     robot.keyPress(KeyEvent.VK_V); //press ctrl v
-		     //robot.delay(450);
+		     robot.delay(2000);
 		     robot.keyRelease(KeyEvent.VK_CONTROL); //release ctrl key
 		     robot.keyRelease(KeyEvent.VK_V); //release ctrlv
-		     //robot.delay(500);
+		     robot.delay(2000);
 		     robot.keyPress(KeyEvent.VK_ENTER); //press enter key
 		     robot.keyRelease(KeyEvent.VK_ENTER); //release enter key 
 		     System.out.println("done"); 
-		   	     
-		   	     //fileupload.sendKeys("C:\\Users\\reshma.s02\\Downloads\\SELEDEMO.xlsx");
+		   	     /*
+		   	     fileupload.sendKeys("C:\\Users\\reshma.s02\\Downloads\\SELEDEMO.xlsx");
 		     
 		     
 		     WebElement acceptbutton=driver.findElement(By.id("terms"));
 		     acceptbutton.click();
 		     WebElement submitbutton= driver.findElement(By.id("submitbutton"));
-		     submitbutton.click();
+		     submitbutton.click();*/
 		   	     
 		   	     
 	}
